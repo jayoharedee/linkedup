90ad856a2b74dbaabd1d694bdddbf0015b32ff68
Jordan Morenstein <jordan@caxap.design>
2018-08-02 11:34:18 -0400

    Deleting an account, there's an action for that now

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -3,8 +3,9 @@ import axios from 'axios';
 import {
   GET_PROFILE,
   PROFILE_LOADING,
-  GET_ERRORS,
   CLEAR_CURRENT_PROFILE,
+  GET_ERRORS,
+  SET_CURRENT_USER,
 } from './types';
 
 export const getCurrentProfile = () => (dispatch) => {
@@ -22,6 +23,36 @@ export const getCurrentProfile = () => (dispatch) => {
         payload: {},
       })
     })
+}
+
+export const createProfile = (profileData, history) => (dispatch) => {
+  axios.post('/api/profile', profileData)
+    .then((res) => history.push('/dashboard'))
+    .catch((err) => {
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data,
+      })
+    })
+}
+
+export const deleteAccount = () => (dispatch) => {
+  if (window.confirm('Are you sure? This can NOT be undone!')) {
+    axios
+      .delete('/api/profile')
+      .then(res =>
+        dispatch({
+          type: SET_CURRENT_USER,
+          payload: {}
+        })
+      )
+      .catch(err =>
+        dispatch({
+          type: GET_ERRORS,
+          payload: err.response.data
+        })
+      );
+  }
 }
 
 export const setProfileLoading = () => {
