cf4633c32d2eb81afba287170416efc307eec03d
Jordan Morenstein <jordan@caxap.design>
2018-08-02 11:33:37 -0400

    Added private routes for CRUD profile actions

--- client/src/App.js
+++ client/src/App.js
@@ -19,6 +19,9 @@ import Dashboard from './components/dashboard/Dashboard';
 
 import PrivateRoute from './components/common/PrivateRoute';
 
+import CreateProfile from './components/create-profile/CreateProfile';
+import EditProfile from './components/edit-profile/EditProfile';
+
 import './App.css';
 
 if (localStorage.jwtToken) {
@@ -49,7 +52,13 @@ class App extends Component {
               <Route exact path="/register" component={ Register } />
               <Route exact path="/login" component={ Login } />
               <Switch>
-              <PrivateRoute exact path="/dashboard" component={ Dashboard } />
+                <PrivateRoute exact path="/dashboard" component={ Dashboard } />
+              </Switch>
+              <Switch>
+                <PrivateRoute exact path="/create-profile" component={ CreateProfile } />
+              </Switch>
+              <Switch>
+                <PrivateRoute exact path="/edit-profile" component={ EditProfile } />
               </Switch>
             </div>
             <Footer />
