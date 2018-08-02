7ca4c600a5278500375e5b372b4d7df1fe45b0e7
Jordan Morenstein <jordan@caxap.design>
2018-08-02 08:42:37 -0400

    Logic to clear profile on logout and setup a route for Dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import { Provider } from 'react-redux';
 import store from './store';
 import setAuthToken from './utils/setAuthToken';
 import { setCurrentUser, logoutUser } from './actions/authActions';
+import { clearCurrentProfile } from './actions/profileActions';
 
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
@@ -13,6 +14,7 @@ import Landing from './components/layout/Landing';
 
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+import Dashboard from './components/dashboard/Dashboard';
 
 import './App.css';
 
@@ -26,6 +28,7 @@ if (localStorage.jwtToken) {
   const currentTime = Date.now() / 1000;
   if (decoded.exp < currentTime) {
     store.dispatch(logoutUser());
+    store.dispatch(clearCurrentProfile());
 
     window.location.href = '/login';
   }
@@ -42,6 +45,7 @@ class App extends Component {
             <div className="container">
               <Route exact path="/register" component={ Register } />
               <Route exact path="/login" component={ Login } />
+              <Route exact path="/dashboard" component={ Dashboard } />
             </div>
             <Footer />
           </div>
