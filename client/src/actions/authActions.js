16f1932079fe704c30324b4954c146cc67fb28de
Jordan Morenstein <jordan@caxap.design>
2018-08-01 23:07:51 -0400

    Auth actions and types created

---
+++ client/src/actions/authActions.js
@@ -0,0 +1,43 @@
+import axios from 'axios';
+import jwt_decode from 'jwt-decode';
+
+import setAuthToken from '../utils/setAuthToken';
+
+import { GET_ERRORS, SET_CURRENT_USER } from './types';
+
+export const registerUser = (userData, history) => (dispatch) => {
+  axios.post('/api/users/register', userData)
+    .then((res) => history.push('/login'))
+    .catch((err) => {
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data,
+      });
+    });
+}
+
+export const loginUser = (userData) => (dispatch) => {
+  axios.post('/api/users/login', userData)
+    .then((res) => {
+      const { token } = res.data;
+
+      localStorage.setItem('jwtToken', token);
+      // Set token to auth header
+      setAuthToken(token);
+      // Decode token to get user data
+      const decoded = jwt_decode(token);
+      // Set current user
+      dispatch(setCurrentUser(decoded));
+    })
+    .catch((err) => {
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data,
+      });
+    });
+}
+
+export const setCurrentUser = (decoded) => ({
+  type: SET_CURRENT_USER,
+  payload: decoded,
+})
