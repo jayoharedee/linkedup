1e67449a06f54a9c0ec1aef76e67cc47e759e27f
Jordan Morenstein <jordan@caxap.design>
2018-08-02 11:33:58 -0400

    fixed input field value bug

--- client/src/components/common/SelectListGroup.js
+++ client/src/components/common/SelectListGroup.js
@@ -1,21 +1,13 @@
-import React from 'react'
+import React from 'react';
 import classnames from 'classnames';
 import PropTypes from 'prop-types';
 
-const SelectListGroup = ({
-  name,
-  placeholder,
-  value,
-  error,
-  info,
-  onChange,
-  options,
-}) => {
-  const selectOptions = options.map((option) => (
+const SelectListGroup = ({ name, value, error, info, onChange, options }) => {
+  const selectOptions = options.map(option => (
     <option key={option.label} value={option.value}>
       {option.label}
     </option>
-  ))
+  ));
   return (
     <div className="form-group">
       <select
@@ -29,18 +21,18 @@ const SelectListGroup = ({
         {selectOptions}
       </select>
       {info && <small className="form-text text-muted">{info}</small>}
-      {error && (<div className="invalid-feedback">{error}</div>)}
+      {error && <div className="invalid-feedback">{error}</div>}
     </div>
-  )
-}
+  );
+};
 
 SelectListGroup.propTypes = {
   name: PropTypes.string.isRequired,
-  onChange: PropTypes.func.isRequired,
   value: PropTypes.string.isRequired,
-  options: PropTypes.array.isRequired,
   info: PropTypes.string,
   error: PropTypes.string,
-}
+  onChange: PropTypes.func.isRequired,
+  options: PropTypes.array.isRequired
+};
 
 export default SelectListGroup;
