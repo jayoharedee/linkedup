af3633bdff1ccd972518dd9d8a6bbedf449344ff
Jordan Morenstein <jordan@caxap.design>
2018-08-04 21:10:59 -0400

    started components and actions for profiles

--- client/src/App.js
+++ client/src/App.js
@@ -26,6 +26,9 @@ import EditProfile from './components/edit-profile/EditProfile';
 import AddExperience from './components/add-credentials/AddExperience';
 import AddEducation from './components/add-credentials/AddEducation';
 
+import Profiles from './components/profiles/Profiles';
+// import AddEducation from './components/add-credentials/AddEducation';
+
 import './App.css';
 
 if (localStorage.jwtToken) {
@@ -55,6 +58,7 @@ class App extends Component {
             <div className="container">
               <Route exact path="/register" component={ Register } />
               <Route exact path="/login" component={ Login } />
+              <Route exact path="/profiles" component={ Profiles } />
               <Switch>
                 <PrivateRoute exact path="/dashboard" component={ Dashboard } />
               </Switch>
