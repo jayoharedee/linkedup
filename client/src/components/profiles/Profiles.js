140e668448b04c93138cf04a0318f8f20dea5d62
Jordan Morenstein <jordan@caxap.design>
2018-08-08 16:56:13 -0400

    profile array needed a pluralized prop for profiles instead of profile

--- client/src/components/profiles/Profiles.js
+++ client/src/components/profiles/Profiles.js
@@ -1,13 +1,12 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
-
+import PropTypes from 'prop-types';
+import Spinner from '../common/Spinner';
+import ProfileItem from './ProfileItem';
 import { getProfiles } from '../../actions/profileActions';
 
-import Spinner from './../common/Spinner';
-
 class Profiles extends Component {
-  componentDidMount = () => {
+  componentDidMount() {
     this.props.getProfiles();
   }
 
@@ -15,13 +14,17 @@ class Profiles extends Component {
     const { profiles, loading } = this.props.profile;
     let profileItems;
 
+    console.log(profiles)
+
     if (profiles === null || loading) {
       profileItems = <Spinner />;
     } else {
       if (profiles.length > 0) {
-        profileItems = <h1>PROFILES HERE</h1>;
+        profileItems = profiles.map(profile => (
+          <ProfileItem key={profile._id} profile={profile} />
+        ));
       } else {
-        profileItems = <h4>No Profles Found</h4>
+        profileItems = <h4>No profiles found...</h4>;
       }
     }
 
@@ -30,9 +33,7 @@ class Profiles extends Component {
         <div className="container">
           <div className="row">
             <div className="col-md-12">
-              <h1 className="display-4 text-center">
-                Developer Profiles
-              </h1>
+              <h1 className="display-4 text-center">Developer Profiles</h1>
               <p className="lead text-center">
                 Browse and connect with developers
               </p>
@@ -41,17 +42,17 @@ class Profiles extends Component {
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
 Profiles.propTypes = {
   getProfiles: PropTypes.func.isRequired,
-  profile: PropTypes.object.isRequired,
-}
+  profile: PropTypes.object.isRequired
+};
 
 const mapStateToProps = (state) => ({
-  profile: state.profile,
+  profile: state.profile
 });
 
 export default connect(mapStateToProps, { getProfiles })(Profiles);
