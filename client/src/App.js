9130fc6f82e5021045d715ecd1aa780146e9dfc5
Jordan Morenstein <jordan@caxap.design>
2018-08-09 14:12:12 -0400

    updated types/actions and nav to work with the new posting component

--- client/src/App.js
+++ client/src/App.js
@@ -31,6 +31,9 @@ import AddEducation from './components/add-credentials/AddEducation';
 import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
 
+import Posts from './components/posts/Posts';
+import Post from './components/post/Post';
+
 import './App.css';
 
 if (localStorage.jwtToken) {
@@ -58,26 +61,48 @@ class App extends Component {
             <Navbar />
             <Route exact path="/" component={ Landing } />
             <div className="container">
-              <Route exact path="/register" component={ Register } />
-              <Route exact path="/login" component={ Login } />
-              <Route exact path="/profiles" component={ Profiles } />
-              <Route exact path="/profile/:handle" component={ Profile } />
+            <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/profiles" component={Profiles} />
+              <Route exact path="/profile/:handle" component={Profile} />
               <Switch>
-                <PrivateRoute exact path="/dashboard" component={ Dashboard } />
+                <PrivateRoute exact path="/dashboard" component={Dashboard} />
               </Switch>
               <Switch>
-                <PrivateRoute exact path="/create-profile" component={ CreateProfile } />
+                <PrivateRoute
+                  exact
+                  path="/create-profile"
+                  component={CreateProfile}
+                />
               </Switch>
               <Switch>
-                <PrivateRoute exact path="/edit-profile" component={ EditProfile } />
+                <PrivateRoute
+                  exact
+                  path="/edit-profile"
+                  component={EditProfile}
+                />
               </Switch>
               <Switch>
-                <PrivateRoute exact path="/add-experience" component={ AddExperience } />
+                <PrivateRoute
+                  exact
+                  path="/add-experience"
+                  component={AddExperience}
+                />
               </Switch>
               <Switch>
-                <PrivateRoute exact path="/add-education" component={ AddEducation } />
+                <PrivateRoute
+                  exact
+                  path="/add-education"
+                  component={AddEducation}
+                />
               </Switch>
-              <Route exact path="/not-found" component={ NotFound } />
+              <Switch>
+                <PrivateRoute exact path="/feed" component={Posts} />
+              </Switch>
+              <Switch>
+                <PrivateRoute exact path="/post/:id" component={Post} />
+              </Switch>
+              <Route exact path="/not-found" component={NotFound} />
             </div>
             <Footer />
           </div>
