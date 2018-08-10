db471b8165223fe56f68ca5454df0ae0f8b3fae0
Jordan Morenstein <jordan@caxap.design>
2018-08-09 21:38:30 -0400

    restyle add-education page

--- client/src/components/add-credentials/AddEducation.js
+++ client/src/components/add-credentials/AddEducation.js
@@ -6,6 +6,19 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { addEducation } from '../../actions/profileActions';
 
+import {
+  Form,
+  Container,
+  Divider,
+  Button,
+  Icon,
+  Header,
+  TextArea,
+ } from 'semantic-ui-react';
+
+
+import Field from '../common/FormField';
+
 class AddEducation extends Component {
   constructor(props) {
     super(props);
@@ -64,56 +77,112 @@ class AddEducation extends Component {
 
     return (
       <div className="add-education">
+        <Container>
+          <Header size='huge' textAlign='center'>
+            <Icon name='settings' />
+            Add Education
+            <Header.Subheader>Add any school, bootcamp, etc that you have attended</Header.Subheader>
+          </Header>
+          <Form>
+            <Field
+              placeholder="* School"
+              name="school"
+              value={this.state.school}
+              onChange={this.onChange}
+              error={errors.school}
+            />
+            <Field
+              placeholder="* Degree or Certification"
+              name="degree"
+              value={this.state.degree}
+              onChange={this.onChange}
+              error={errors.degree}
+            />
+            <Field
+              placeholder="* Field of Study"
+              name="fieldofstudy"
+              value={this.state.fieldofstudy}
+              onChange={this.onChange}
+              error={errors.fieldofstudy}
+            />
+            <Field
+              name="from"
+              type="date"
+              value={this.state.from}
+              onChange={this.onChange}
+              error={errors.from}
+            />
+            <Field
+              name="to"
+              type="date"
+              value={this.state.to}
+              onChange={this.onChange}
+              error={errors.to}
+              disabled={this.state.disabled ? 'disabled' : ''}
+            />
+            <TextArea
+              autoHeight
+              placeholder="Program Description"
+              name="description"
+              value={this.state.description}
+              onChange={this.onChange}
+              error={errors.description}
+              info="Tell us about the program that you were in"
+            />
+          </Form>
+          <Divider />
+          <Button color='black' onClick={this.onSubmit}>Submit</Button>
+        </Container>
         <div className="container">
           <div className="row">
             <div className="col-md-8 m-auto">
               <Link to="/dashboard" className="btn btn-light">
                 Go Back
               </Link>
-              <h1 className="display-4 text-center">Add Education</h1>
-              <p className="lead text-center">
+              {/* <h1 className="display-4 text-center">Add Education</h1> */}
+              {/* <p className="lead text-center">
                 Add any school, bootcamp, etc that you have attended
-              </p>
+              </p> */}
               <small className="d-block pb-3">* = required fields</small>
               <form onSubmit={this.onSubmit}>
-                <TextFieldGroup
+                {/* <TextFieldGroup
                   placeholder="* School"
                   name="school"
                   value={this.state.school}
                   onChange={this.onChange}
                   error={errors.school}
-                />
-                <TextFieldGroup
+                /> */}
+                {/* <TextFieldGroup
                   placeholder="* Degree or Certification"
                   name="degree"
                   value={this.state.degree}
                   onChange={this.onChange}
                   error={errors.degree}
-                />
-                <TextFieldGroup
+                /> */}
+                {/* <TextFieldGroup
                   placeholder="* Field of Study"
                   name="fieldofstudy"
                   value={this.state.fieldofstudy}
                   onChange={this.onChange}
                   error={errors.fieldofstudy}
-                />
+                /> */}
                 <h6>From Date</h6>
-                <TextFieldGroup
+                {/* <TextFieldGroup
                   name="from"
                   type="date"
                   value={this.state.from}
                   onChange={this.onChange}
                   error={errors.from}
-                />
+                /> */}
                 <h6>To Date</h6>
-                <TextFieldGroup
+                {/* <TextFieldGroup
                   name="to"
                   type="date"
                   value={this.state.to}
                   onChange={this.onChange}
                   error={errors.to}
                   disabled={this.state.disabled ? 'disabled' : ''}
-                />
+                /> */}
                 <div className="form-check mb-4">
                   <input
                     type="checkbox"
@@ -125,17 +194,17 @@ class AddEducation extends Component {
                     id="current"
                   />
                   <label htmlFor="current" className="form-check-label">
-                    Current Job
+                    Current School
                   </label>
                 </div>
-                <TextAreaFieldGroup
+                {/* <TextAreaFieldGroup
                   placeholder="Program Description"
                   name="description"
                   value={this.state.description}
                   onChange={this.onChange}
                   error={errors.description}
                   info="Tell us about the program that you were in"
-                />
+                /> */}
                 <input
                   type="submit"
                   value="Submit"
